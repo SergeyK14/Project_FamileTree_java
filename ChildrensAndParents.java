8000c9eb1b1cae604175eb3653dfa14e6cd18bf5
SergeyK14 <serega_konchakov@mail.ru>
2023-04-18 08:22:33 +0300

    Backup file

---
+++ ChildrensAndParents.java
@@ -0,0 +1,33 @@
+// package Project FamilyTree;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+
+// public class ChildrensAndParents {
+
+//     private List<Human> childrens;
+//     private List<Human> parents;
+    
+//     public Childrens (List<Human> childrens){
+//         this.childrens = childrens;
+//     }
+//     public ChildrensAndParents (){
+//         childrens = new ArrayList<>();
+//     }
+//     public void addChildren (Human human){
+//         childrens.add (human);
+//     }
+
+//     public Parents  (List<Human> parents){
+//         this.parents = parents;
+//     }
+//     public ChildrensAndParents (){
+//         childrens = new ArrayList<>();
+//     }
+//     public void addChildren (Human human){
+//         childrens.add (human);
+//     }
+
+
+// }
